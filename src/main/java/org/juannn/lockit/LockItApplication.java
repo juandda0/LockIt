9a86abbe2756ef43229d0c60a663fac346a22ea2
juandda0 <juandiegodiazave@gmail.com>
2025-05-25 12:01:20 -0500

    added restController passwordController

--- src/main/java/org/juannn/lockit/LockItApplication.java
+++ src/main/java/org/juannn/lockit/LockItApplication.java
@@ -3,11 +3,17 @@ package org.juannn.lockit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.security.SecureRandom;
+
 @SpringBootApplication
 public class LockItApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LockItApplication.class, args);
+        SecureRandom random = new SecureRandom();
+        int password = random.nextInt(1000000000);
+        System.out.println("Contraseña: " + password);
+
     }
 
 }
---
+++ src/main/java/org/juannn/lockit/controller/PasswordController.java
@@ -0,0 +1,13 @@
+package org.juannn.lockit.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/password")
+public class PasswordController {
+
+
+
+
+}
