f49d5e1ef9348a53ee5c24b8365a488d1fc9534c
juandda0 <juandiegodiazave@gmail.com>
2025-05-25 11:16:21 -0500

    initialize layared folder structure

---
+++ src/main/java/org/juannn/lockit/LockItApplication.java
@@ -0,0 +1,13 @@
+package org.juannn.lockit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LockItApplication.class, args);
+    }
+
+}
