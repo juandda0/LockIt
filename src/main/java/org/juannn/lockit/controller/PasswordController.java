3115aaf867f0eb2a8a467e841b6cbbcbdf9485d4
juandda0 <juandiegodiazave@gmail.com>
2025-05-25 17:12:03 -0500

    new entity user

--- src/main/java/org/juannn/lockit/controller/PasswordController.java
+++ src/main/java/org/juannn/lockit/controller/PasswordController.java
@@ -1,11 +1,11 @@
 package org.juannn.lockit.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/password")
 public class PasswordController {
-
 
 }
---
+++ src/main/java/org/juannn/lockit/model/User.java
@@ -0,0 +1,26 @@
+package org.juannn.lockit.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Entity
+@Table(name = "Users")
+public class User {
+
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+}
